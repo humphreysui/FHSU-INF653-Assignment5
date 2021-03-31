aa3ea87398d4aae7cab714bae412fe3230da2362
Humphrey SUI <55611676+humphreysui@users.noreply.github.com>
2021-03-31 22:30:27 +0800

    .

---
+++ admin/view/footer.php
@@ -0,0 +1,20 @@
+</main>
+<div class="divider"></div>
+<footer class="page-footer footer-copyright container white section">
+  <div class="center-align black-text">
+    &copy; 2021 Zippy Used Auto by Humphrey Sui, Qiupeng (admin).
+  </div> 
+
+</footer>
+
+<script>
+  $(document).ready(function () {
+    $('select').formSelect();
+    $('.tabs').tabs();
+    /* $(".dropdown-trigger").dropdown(); */
+  });
+
+</script>
+
+</body> 
+</html>
